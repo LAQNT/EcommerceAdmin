01ae7db485fd9a0d3e3693fcd85d71b3b62afa12
LAQNT <laura.quintero.a@hotmail.com>
2023-08-17 13:02:17 +0200

    navbar ok

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,5 +1,20 @@
-const DashboardPage = () => {
-  return <div>DASHBOARD</div>;
+import prismadb from "@/lib/prismadb";
+
+interface DashboardPageProps {
+  params: { storeId: string };
+}
+
+const DashboardPage: React.FC<DashboardPageProps> = async ({ params }) => {
+  const store = await prismadb.store.findFirst({
+    where: {
+      id: params.storeId,
+    },
+  });
+  return (
+    <div>
+      Active Store: <strong>{store?.name}</strong>{" "}
+    </div>
+  );
 };
 
 export default DashboardPage;
--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -40,7 +40,8 @@ export const StoreModal = () => {
       setLoading(true);
 
       const response = await axios.post("/api/stores", values);
-      toast.success("Store created");
+      window.location.assign(`/${response.data.id}`);
+      // toast.success("Store created");
     } catch (error) {
       toast.error("Something went wrong.");
     } finally {
